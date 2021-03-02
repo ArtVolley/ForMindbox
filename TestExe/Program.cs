84205335ed7f803711b2fa95e4b32ed38e73cf1c
Артём Усвяцов <artoumy@yandex.ru>
2021-03-02 21:47:16 +0300

    first commit

---
+++ TestExe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using figureSquare;
+
+namespace TestExe
+{
+    class Program
+    {
+        static void Main()
+        {
+            Figure t = new Figure(3, 4, 5);
+            double ttt = t.Square();
+            Console.WriteLine(t.Square());
+            Figure p = new Figure(2);
+            double ppp = p.Square();
+            Console.WriteLine(t.IsRightTriangle());
+            Console.WriteLine(p.Square());
+            
+        }
+    }
+}
---
+++ figureSquare/Figure.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace figureSquare
+{
+    public class Figure
+    {
+        public int radius;
+        public int a;
+        public int b;
+        public int c;
+        public bool isItCircle = false;
+        public bool isItTriangle = false;
+
+        public Figure(int radius) // создание круга 
+        {
+            this.radius = radius;
+            this.isItCircle = true;
+        }
+
+        public Figure(int a, int b, int c) // создание треугольника
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.isItTriangle = true;
+        }
+
+
+        public double Square() // площадь 
+        {
+            double square = 0;
+            if (isItTriangle == true) // если треугольник
+            {
+                int semper = (a + b + c) / 2;
+                square = Math.Sqrt(semper * (semper - a) * (semper - b) * (semper - c));
+            }
+            else if (isItCircle == true) // если круг
+            {
+                square = Math.PI * radius * radius;
+            }
+            return square;
+        }
+
+        public bool IsRightTriangle() // проверка на прямоугольность треугольника
+        {
+            bool isRightTriangle = false;
+            if (isItTriangle == true)
+            {
+                if (a * a == b * b + c * c) // т. Пифагора
+                {
+                    isRightTriangle = true;
+                }
+                else if (b * b == a * a + c * c)
+                {
+                    isRightTriangle = true;
+                }
+                else if (c * c == b * b + a * a)
+                {
+                    isRightTriangle = true;
+                }
+            }
+            return isRightTriangle;
+        }
+    }
+}
